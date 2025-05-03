a1d8faadcaec1c0a80c65786a74d57fab68080bc
Entreri1406 <jerry.aiquipa@pucp.edu.pe>
2025-05-03 18:24:07 -0500

    Adicion de la carpeta componentes para modularizar las funciones

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Componentes en react en mayúscualas y HTML en minúsculas
+// No es recomendable crear varios componentes en el mismo archivo. Por fines didácticos se hace así, pero luego se modulizará.
 
-function App() {
-  const [count, setCount] = useState(0)
+// En Vite podemos importar imágenes directamente desde la carpeta src/assets/images/ejemplo-img.jpg
+import img1 from './assets/images/ejemplo-img.jpg';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+const MyButton = ({ text }) => { // props es un objeto que contiene todas las propiedades que le pasamos al componente. Se puede desestructurar para obtener las propiedades que necesitamos.
+    const handleClickButton = (title) => {
+        console.log("Soy un click " + title);
+    }
+    return ( // La función en onClick se ejecuta al dar click al botón. Pero, si quisieramos pasarle un argumento, se debe usar una función anónima o una función flecha.
+        <button onClick={() => handleClickButton(text)}>
+            {text}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
 
-export default App
+const WelcomeText = ({ user }) => user ? <h2>Online</h2> : <h2>Offline</h2>;
+
+const ItemFruta = (props) => {
+    return (
+        <li>
+            {props.fruta}
+        </li>
+    )
+};
+
+
+const App = () => {
+
+    const title = "Hola soy React-desde title"; // Variable de tipo string
+    const user = false;
+    const frutas = ["🍐", "🍎", "🍉"];
+
+    return (
+        // Los componentes HTML deben estar dentro de un contenedor al usarse ()
+        <>
+            <h1 className="text-center">{title}</h1>
+            <img src={img1} alt="Imagen de prueba React JS" />
+            <MyButton text="boton-1" />
+            <MyButton text="boton-2" />
+            <MyButton text="boton-3" />
+            <WelcomeText user={user} />
+            <ul>
+                {
+                    frutas.map((item, index) => {
+                        return (
+                            <ItemFruta key={index} fruta={item} />
+                        )
+                    })
+                }
+            </ul>
+        </>
+    ); /* Export un archivo JSX. Está más cerca de JS que de HTML */
+}
+export default App;
